f6f20d4d85fa1e12c9b5e34453ab6f25d7fe26a8
anemorin <arknahon@gmail.com>
2023-07-03 14:01:35 +0300

    make navigation and bottom menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/summerpractice/app_project/MainActivity.kt
+++ app/src/main/java/com/summerpractice/app_project/MainActivity.kt
@@ -2,10 +2,19 @@ package com.summerpractice.app_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val controller = (supportFragmentManager.findFragmentById(R.id.fcv_container)
+                as NavHostFragment).navController
+        findViewById<BottomNavigationView>(R.id.bnv_menu).apply {
+            setupWithNavController(controller)
+        }
     }
 }
