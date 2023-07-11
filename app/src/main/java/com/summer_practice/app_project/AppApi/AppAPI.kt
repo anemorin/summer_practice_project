bc4db7071f66bfa9f9309e7e7b322c7f9f6493ba
David <limonchik087@gmail.com>
2023-07-11 19:03:50 +0300

    add Authorization

--- app/src/main/java/com/summer_practice/app_project/AppApi/AppAPI.kt
+++ app/src/main/java/com/summer_practice/app_project/AppApi/AppAPI.kt
@@ -1,6 +1,7 @@
 package com.summer_practice.app_project.AppApi
 
 import org.json.JSONObject
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -16,7 +17,7 @@ interface AppAPI {
 
     @POST("/auth/login")
     @Headers("Content-Type: application/json")
-    suspend fun logIn(@Body creds: LogInAtr) : String
+    suspend fun logIn(@Body creds: LogInAtr) : Response<UserItem>
 
     @GET("/manga/?title={title}")
     suspend fun searchManga(@Path("title") title: String) : ApiMultiItem
--- app/src/main/java/com/summer_practice/app_project/ProfileFragment.kt
+++ app/src/main/java/com/summer_practice/app_project/ProfileFragment.kt
@@ -6,16 +6,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.NavHostFragment
+import com.summer_practice.app_project.AppApi.AppAPI
+import com.summer_practice.app_project.AppApi.LogInAtr
+import com.summer_practice.app_project.databinding.FragmentMyLibraryBinding
+import com.summer_practice.app_project.databinding.FragmentProfileBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        val retroFit = Retrofit.Builder()
+            .baseUrl("https://api.mangadex.org")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        val service = retroFit.create(AppAPI::class.java)
 
         val sharedPreferences = activity?.getSharedPreferences("APP",Context.MODE_PRIVATE);
         if (sharedPreferences != null) {
             if (sharedPreferences.getString("token", null).isNullOrEmpty()) {
-            }
+                binding.button.setOnClickListener{
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val user = service.logIn(
+                            LogInAtr(
 
+                                binding.login.text.toString(),
+                                binding.password.text.toString(),
+
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 }
